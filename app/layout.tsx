b27f4e74081a5449b77968087355fde8bff9ebce
DavBN <162839191+DavBN@users.noreply.github.com>
2024-07-10 10:32:52 -0500

    Creacion de rutas para loggin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from 'nextjs-toploader';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const outfit = Outfit({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Renta de Carros",
   description: "Creado por Oscar Gallego",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+      <NextTopLoader color="#000"/>
+        {children}
+      </body>
     </html>
+    </ClerkProvider>
   );
 }
