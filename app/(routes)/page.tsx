1ade3287848ce651cbcf47a3a613f94caf2e91bd
DavBN <162839191+DavBN@users.noreply.github.com>
2024-07-13 09:32:36 -0500

    Creacion de más componentes para la parte del menú del administrador

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div>
+  <div className="h-full flex items-center justify-center">
+    Page
+  </div>
+ </div>
+  );
+}
